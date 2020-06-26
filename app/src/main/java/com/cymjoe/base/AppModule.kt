920308c80868bc5be1efe6a331380813e8a46636
cuiyl <cym210724>
2020-06-26 18:22:11 +0800

    第二次提交

---
+++ app/src/main/java/com/cymjoe/base/AppModule.kt
@@ -0,0 +1,31 @@
+package com.cymjoe.base
+
+import com.cymjoe.base.ui.activity.main.MainViewModel
+import org.koin.androidx.viewmodel.dsl.viewModel
+import org.koin.dsl.module
+
+
+val viewModelModule = module {
+    viewModel { MainViewModel() }
+
+//    viewModel { ArticleViewModel(get(), get(), get(), get(), get()) }
+//    viewModel { SystemViewModel(get(), get()) }
+//    viewModel { NavigationViewModel(get()) }
+//    viewModel { ProjectViewModel(get()) }
+//    viewModel { SearchViewModel(get(), get()) }
+}
+
+val repositoryModule = module {
+    //    single { WanRetrofitClient.getService(WanService::class.java, WanService.BASE_URL) }
+//    single { CoroutinesDispatcherProvider() }
+//    single { LoginRepository(get()) }
+//    single { SquareRepository() }
+//    single { HomeRepository() }
+//    single { ProjectRepository() }
+//    single { CollectRepository() }
+//    single { SystemRepository() }
+//    single { NavigationRepository() }
+//    single { SearchRepository() }
+}
+
+val appModule = listOf(viewModelModule, repositoryModule)
