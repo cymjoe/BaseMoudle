417cdba60bb05ca156f9dfa6cf9051e6fc500a2f
钟离 <574774295@qq.com>
2020-07-09 14:35:19 +0800

    1

--- app/src/main/java/com/cymjoe/work/ui/activity/main/MainViewModel.kt
+++ app/src/main/java/com/cymjoe/work/ui/activity/main/MainViewModel.kt
@@ -11,15 +11,7 @@ class MainViewModel : BaseViewModel() {
     private val api = NetManager.createRetrofit(Constant.BASE_URL).create(ApiService::class.java)
     fun login() {
         launch {
-            val dataConvert = api.getVisitList(
-                "eyJhbGciOiJIUzUxMiJ9.eyJ0ZW5jZW50U2lnIjoiZUp3MWpsMExnakFVaHYtTGJndlpXY2M1aFM2eWkwcGlmU3lLTG9Y" +
-                        "Tk9GUm1KaVZFLTcyaGRmbCpQYnh2dGx1YXdMVVYxWTRsSVNqa25BODc4KmxxbGpBUmNOYnJoejNuVlVXV0plQkxJREdXVVoqUWRXVkRCWFVEUk" +
-                        "9DaFVBb0FwUnh4UUl6KkFEcjUzS2lKMk43MUM5VWxiWTlOVkI0V2V6MDNobUE2V0tlYWlwbk9lTGJLaTl0bS1CczJkUFhuSUl4UlNPbkJueS1x" +
-                        "RWpKUiIsInBlb3BsZUlkIjoiNDQxMDUyODgxMTUwOTMwOTQ0IiwiY3JlYXRlZCI6MTU5NDI2NjgxMTc3MiwiYWNjaWQiOiI0NDEwNTI4ODExND" +
-                        "Y2MzAxNDQ3IiwiZXhwIjoxNTk5NDUwODExLCJ1c2VySWQiOiI0NDEwNTI4ODExNjMzODY4ODAifQ.0w0X4NYVf4FQJ6SJE4zdze_FlZNp4U8T9" +
-                        "h_Nz8R-62Viog1t-xjjgupjQtqNmUXuidWlbfSsi5sF30JDSBr79w"
-            ).dataConvert()
-            Log.d("PPPPPP", dataConvert.toString())
+
         }
     }
 }
